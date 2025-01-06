a4cbccf4883afabbac4951efcddef683f6670c3f
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-01-06 14:10:31 +0530

    CORS uupdate

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 
 const app = express();
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://togetherable.vercel.app'],
-}));
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
